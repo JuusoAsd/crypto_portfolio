fdcaca231ead7427f9a193d57f89da526e1cfc00
JuusoAsd <jahlroos96@gmail.com>
2021-10-07 14:07:15 +0300

    remove excess, formatting

--- src/transaction.py
+++ src/transaction.py
@@ -8,6 +8,3 @@ class Tx():
         self.amount_principal = amount_principal
         self.total_price = amount_usd
         self.unit_price = amount_usd /amount_principal
-        #self.price = amount_usd /amount_principal
-
-
